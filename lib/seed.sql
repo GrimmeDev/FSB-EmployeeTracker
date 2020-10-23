e905d595ebcb510ed90c847b34518db07da703f6
Robert Grimes <rjgrimes@gmail.com>
2020-10-23 18:23:59 -0500

    Adjusted database funcitonality and seed values

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,14 +1,9 @@
---Initial database creation
-DROP DATABASE IF EXISTS department_db;
-DROP DATABASE IF EXISTS role_db;
-DROP DATABASE IF EXISTS employee_db;
+-- Initial database creation
+DROP DATABASE IF EXISTS company_DB;
+CREATE DATABASE company_DB;
 
-CREATE DATABASE department_db;
-CREATE DATABASE role_db;
-CREATE DATABASE employee_db;
-
---Creation of tables in databases
-USE department_db;
+-- Creation of tables in databases
+USE company_DB;
 CREATE TABLE departments(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
@@ -17,28 +12,28 @@ CREATE TABLE departments(
 INSERT INTO departments(name)
 VALUES("IT"),("HR"),("DEV");
 
-USE role_db;
 CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (department_id) INT REFERENCES(departments)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 INSERT INTO roles(title, salary, department_id)
 VALUES("Developer", 34000, 3),("Dev Lead", 42000, 3),
 ("Technician", 30000, 1),("SysAdmin", 50000, 1),
-("Manager", 25000,3),("Coordinator", 38000, 3);
+("Manager", 25000, 3),("Coordinator", 38000, 3);
 
-USE employee_db;
 CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY(role_id) INT REFERENCES(roles),
+    roles_id INT,
+    FOREIGN KEY(roles_id) REFERENCES roles(id),
     manager_id INT NULL,
-    CONSTRAINT FOREIGN KEY(manager_id) REFERENCES(employees)
+    FOREIGN KEY(manager_id) REFERENCES employees(id)
 );
 
-INSERT INTO employees(first_name, last_name, role_id)
-VALUES
+INSERT INTO employees(first_name, last_name, roles_id, manager_id)
+VALUES("Bob", "Sagget", 4, NULL),("Test","Last", 3, 1);
