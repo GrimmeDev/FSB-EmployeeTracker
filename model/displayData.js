173a69736dcf5e9f9481d09462f1313fdd794a90
Robert Grimes <rjgrimes@gmail.com>
2020-10-23 23:07:09 -0500

    Added function to display employees by department

--- model/displayData.js
+++ model/displayData.js
@@ -1,12 +1,13 @@
 function viewAllEmployess(connection) {
     return new Promise((resolve, reject) => {
-        let query = "SELECT emp.id, emp.first_name, emp.last_name, roles.title, departments.name AS department,";
-        query += ' roles.salary, CONCAT(mng.first_name," ", mng.last_name) AS manager';
-        query += " FROM employees AS emp INNER JOIN roles ON emp.roles_id = roles.id";
-        query += " INNER JOIN departments ON roles.department_id = departments.id";
-        query += " LEFT JOIN employees AS mng ON emp.manager_id = mng.id";
-        connection.query(query,
-            (err, data) => {
+        let sqlQuery = "SELECT emp.id, emp.first_name, emp.last_name, roles.title, departments.name AS department,";
+        sqlQuery += " roles.salary, CONCAT(mng.first_name,' ', mng.last_name) AS manager";
+        sqlQuery += " FROM employees AS emp INNER JOIN roles ON emp.roles_id = roles.id";
+        sqlQuery += " INNER JOIN departments ON roles.department_id = departments.id";
+        sqlQuery += " LEFT JOIN employees AS mng ON emp.manager_id = mng.id";
+        // console.log(sqlQuery);
+        connection.query(sqlQuery,
+            function (err, data) {
                 if (err)
                     reject(err);
                 else
@@ -15,13 +16,13 @@ function viewAllEmployess(connection) {
     });
 };
 
-function viewEmpsByDepartment(connection) {
+function viewEmpsByDepartment(connection, department) {
     return new Promise((resolve, reject) => {
         let query = 'SELECT emp.id, emp.first_name, emp.last_name, roles.title, departments.name AS department, CONCAT(mng.first_name, " ", mng.last_name) AS manager';
-        query += "FROM employees AS emp INNER JOIN roles ON emp.roles_id = roles.id";
-        query += "INNER JOIN departments on roles.department_id = departments.id LEFT JOIN employees AS mng ON emp.manager_id = mng.id";
-        query += "WHERE departments.name = ?";
-        connection.query(query, [department], (err, data) => {
+        query += " FROM employees AS emp INNER JOIN roles ON emp.roles_id = roles.id";
+        query += " INNER JOIN departments on roles.department_id = departments.id LEFT JOIN employees AS mng ON emp.manager_id = mng.id";
+        query += " WHERE departments.name = ?";
+        connection.query(query, department, function (err, data) {
             if (err)
                 reject(err);
             else
