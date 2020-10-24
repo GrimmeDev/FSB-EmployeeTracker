bde6adfe2fb20ecb05ca0cb56645137fc288b5f1
Robert Grimes <rjgrimes@gmail.com>
2020-10-24 10:35:48 -0500

    Added funciton to grab role ID from role title

---
+++ questions/askForRole.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquirer");
+
+function displayRoles(connection) {
+    return new Promise((resolve, reject) => {
+        let sqlQuery = "SELECT title FROM roles";
+
+        connection.query(sqlQuery, function (err, data) {
+            if (err)
+                reject(err);
+            else
+                resolve(data);
+        });
+    });
+};
+
+function selectRoles(roles) {
+    // console.log("In select Roles: " + roles);
+    roleList = roles.map(el => el.title);
+    // console.log("In select Roles: " + roleList);
+    return inquirer.prompt([{
+        type: "list",
+        message: "Select a Role:",
+        name: "role",
+        choices: roleList
+    }]);
+};
+
+function getRoleID(connection, role) {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT id FROM roles WHERE title = ?", role.role, function (err, data) {
+            if (err)
+                reject(err);
+            else
+                resolve(data);
+        })
+    })
+}
+
+module.exports = {
+    displayRoles,
+    selectRoles,
+    getRoleID
+}
