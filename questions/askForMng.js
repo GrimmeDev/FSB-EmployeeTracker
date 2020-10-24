b2f596d98bb166559fcae3c1f2a4e1a7ab6d090c
Robert Grimes <rjgrimes@gmail.com>
2020-10-23 23:47:10 -0500

    Moved functions from displayData regarding managers into this file for clarity of files

---
+++ questions/askForMng.js
@@ -0,0 +1,27 @@
+const inquirer = require("inquirer");
+
+function displayManagers(connection) {
+    return new Promise((resolve, reject) => {
+        let sqlQuery = "SELECT CONCAT(emp.first_name, ' ', emp.last_name) AS name FROM employees AS emp WHERE manager_id IS NULL";
+        connection.query(sqlQuery, function (err, data) {
+            if (err)
+                reject(err);
+            else
+                resolve(data);
+        });
+    });
+};
+
+function selectManager(mngs) {
+    return inquirer.prompt([{
+        type: "list",
+        message: "Select a Manager:",
+        name: "mng",
+        choices: mngs
+    }]);
+};
+
+module.exports = {
+    displayManagers,
+    selectManager
+};
