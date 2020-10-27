665cfa5c033eb6d8af6da0fc9ad3da4239702623
Robert Grimes <rjgrimes@gmail.com>
2020-10-27 09:41:53 -0500

    Adjusted whitespace

--- questions/askForRole.js
+++ questions/askForRole.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 function displayRoles(connection) {
     return new Promise((resolve, reject) => {
         let sqlQuery = "SELECT title FROM roles";
-
         connection.query(sqlQuery, function (err, data) {
             if (err)
                 reject(err);
